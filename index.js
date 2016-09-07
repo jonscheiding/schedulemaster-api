b39af443bf6413092857f6635cb33b3020224901
Jon Scheiding <jonscheiding@gmail.com>
2016-09-07 18:04:44 -0400

    Added an unhandled rejection handler

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var path = require('path')
 require('app-module-path').addPath(path.resolve(__dirname, 'src'))
 require('server')
+
+process.on('unhandledRejection', function(reason, p) {
+  console.log('Unhandled ', p)
+})
