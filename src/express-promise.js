ca7a58a0a405f9cf09bb2cb877534e4718e6db0e
Jon Scheiding <jonscheiding@gmail.com>
2016-09-08 21:55:33 -0400

    Small fix so express-promise doesn't do stupid crap if you call .promise() twice

--- src/express-promise.js
+++ src/express-promise.js
@@ -1,12 +1,19 @@
-const resolve = res => result => {
+const resolved = res => result => {
   if(result) res.send(result)
 }
 
-const reject = res => err => {
-  res.status(500).send({error: err.toString()})
+const rejected = res => err => {
+  res.status(500)
+  res.send({error: err.toString()})
 }
 
 export default () => (req, res, next) => {
-  res.promise = (p) => p.then(resolve(res)).catch(reject(res))
+  let promise
+  res.promise = (p) => promise = p
+  
   next()
+  
+  if(promise) {
+    promise.then(resolved(res)).catch(rejected(res))
+  }
 }
