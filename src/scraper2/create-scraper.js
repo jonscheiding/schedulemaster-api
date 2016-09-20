e5e0f9850990a0241a4ea1fb06acff3ce859f8d7
Jon Scheiding <jonscheiding@gmail.com>
2016-09-20 16:11:04 -0400

    Fixed some enhancer functionality and added more tests

--- src/scraper2/create-scraper.js
+++ src/scraper2/create-scraper.js
@@ -1,10 +1,17 @@
+import arrayWrap from 'array-wrap'
 import extend from 'deep-extend'
+
 import request from './request-wrapper'
 
 const cleanOptions = optionsOrUrl => 
   typeof(optionsOrUrl) === 'string' ? {url: optionsOrUrl} : optionsOrUrl
 
-const noop = arg => arg
+const applyEnhancers = (enhancers, select, apply) => {
+  arrayWrap(enhancers).forEach(enhancer => {
+    const fn = select(enhancer)
+    if(fn) apply(fn)
+  })
+}
 
 const createScraper = (defaultOptions, enhancers = []) => {
   const scraper = (options) => {
@@ -13,20 +20,26 @@ const createScraper = (defaultOptions, enhancers = []) => {
       cleanOptions(options)
     )
         
-    enhancers.forEach(enhancer =>
-      finalOptions = (enhancer.options || noop)(finalOptions) || finalOptions
+    applyEnhancers(enhancers, 
+      enhancer => enhancer.options,
+      fn => finalOptions = fn(finalOptions) || finalOptions
     )
         
     let result = request(finalOptions)
     
-    enhancers.forEach(enhancer => 
-      result = result.then(enhancer.result || noop)
+    applyEnhancers(enhancers,
+      enhancer => enhancer.result,
+      fn => result = result.then(r => fn(r, finalOptions) || r)
     )
     
     return result
   }
   
-  scraper.enhance = enhancer => createScraper(defaultOptions, [...enhancers, enhancer])
+  ;['post', 'get'].forEach(method => {
+    scraper[method] = options => scraper({method, ...options})
+  })
+  
+  scraper.enhance = newEnhancers => createScraper(defaultOptions, [...enhancers, ...arrayWrap(newEnhancers)])
   return scraper
 }
 
--- test/scraper/create-scraper.test.js
+++ test/scraper/create-scraper.test.js
@@ -39,20 +39,23 @@ describe('scraper', () => {
     })
   })
   
-  it('should call the options enhancer before making the request', () => {
-    let spy = chai.spy()
+  it('should pass the provided options to the options enhancer', () => {
+    let spiedOptions
+    let spy = chai.spy(options => spiedOptions = options)
     
     let scraper2 = scraper.enhance({
       options: spy
     })
 
-    const promise = scraper2()
+    const promise = scraper2({someProp: 'someValue'})
     
     return promise.then(() => {
       expect(spy).to.have.been.called()
+      expect(spiedOptions).to.have.property('url', 'http://nowhere/test')
+      expect(spiedOptions).to.have.property('someProp', 'someValue')
     })
   })
-  
+
   it('should use the options returned by the options enhancer', () => {
     let scraper2 = createScraper('http://www.google.com').enhance({
       options: () => ({url: 'http://nowhere/test'})
@@ -62,5 +65,38 @@ describe('scraper', () => {
     
     return promise
   })
-
+  
+  it('should call the response enhancer before returning the response', () => {
+    let spiedResult
+    let spiedOptions
+    let spy = chai.spy((result, options) => {
+      spiedResult = result
+      spiedOptions = options
+      return undefined
+    })
+    
+    let scraper2 = scraper.enhance({
+      result: spy
+    })
+    
+    const promise = scraper2()
+    
+    return promise.then(result => {
+      expect(spy).to.have.been.called()
+      expect(spiedResult).to.be.equal(result)
+      expect(spiedOptions).to.have.property('url', 'http://nowhere/test')
+    })
+  })
+  
+  it('should return the response as enhanced by the response enhancer', () => {
+    let scraper2 = scraper.enhance({
+      result: result => ({...result, enhanced: true})
+    })
+    
+    const promise = scraper2()
+    
+    return promise.then(result => {
+      expect(result).to.have.property('enhanced', true)
+    })
+  })
 })
