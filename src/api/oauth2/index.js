0275f43bad6ddbf7648b2a8f4314613d16eef45d
Jon Scheiding <jonscheiding@gmail.com>
2016-09-26 12:18:32 -0400

    Renamed a file to reduce confusingness

--- src/api/oauth2/index.js
+++ src/api/oauth2/index.js
@@ -1,39 +1,22 @@
 import bodyParser from 'body-parser'
 import express from 'express'
-import path from 'path'
 
-import server from './server'
+import oauth2orize from './oauth2orize'
 import { authenticateToken, authenticateClient } from './authenticate'
-import { loginPage } from 'pages'
 
 const api = express.Router()
 api.use(bodyParser.urlencoded({extended: false}))
 
 api.use('/oauth2/token',
   authenticateClient(),
-  server.token(), 
-  server.errorHandler()
+  oauth2orize.token(), 
+  oauth2orize.errorHandler()
 )
 
 api.get('/oauth2/check',
   authenticateToken(),
   (req, res) => res.send(req.token)
 )
-
-api.get('/login', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'login.html')))
-  
-api.post('/login', (req, res) => {
-  loginPage.post({username: req.body.username, password: req.body.password})
-    .then(result => {
-      if(!result) {
-        res.redirect('/login?failed')
-        return
-      }
-      
-      res.send(result)
-    })
-})
 
 export default api
 export { api, authenticateToken, authenticateClient }
