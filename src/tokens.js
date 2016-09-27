f62747eaf06680b162781e00e25f5cd0463d34e8
Jon Scheiding <jonscheiding@gmail.com>
2016-09-27 11:45:29 -0400

    #15 Did some cleaning up to handle token generation better

---
+++ src/tokens.js
@@ -0,0 +1,141 @@
+import arrayWrap from 'array-wrap'
+import createEncrypter from 'object-encrypter'
+import { cleanEnv, str, num } from 'envalid'
+import jwt from 'jsonwebtoken'
+import yup from 'yup'
+
+const yupx = {
+  scope: () => yup.array().of(yup.string().required())
+}
+
+const env = cleanEnv(process.env, { 
+  TOKEN_SECRET: str(),
+  TOKEN_EXPIRATION: num({default: -1})
+})
+
+class Token {
+  constructor(secret = env.TOKEN_SECRET, options = {}) {
+    this.secret = secret
+    this.encrypter = createEncrypter(secret)
+    this.options = {
+      schema: yup.mixed(),
+      ...options,
+      encryptProps: arrayWrap(options.encryptProps)
+    }
+    
+    if(this.options.expiresIn == -1) delete this.options.expiresIn
+    
+    this.parse = this.parse.bind(this)
+    this.stringify = this.stringify.bind(this)
+  }
+  
+  calculateExpiration(expiresIn) {
+    expiresIn = expiresIn || this.options.expiresIn
+    const now = Math.floor(Date.now() / 1000)
+
+    if(expiresIn) return { expires: now + expiresIn, expiresIn }
+    return {}
+  }
+  
+  encryptOrDecryptProps(token, op) {
+    if(op != 'encrypt' && op != 'decrypt') 
+      throw new Error(`Invalid argument ${op} for 'op'.`)
+    
+    if(!this.options.encryptProps) return token
+    const transformed = {}
+    
+    for(let encryptProp of this.options.encryptProps) {
+      if(!token.hasOwnProperty(encryptProp)) continue
+      transformed[encryptProp] = this.encrypter[op](token[encryptProp])
+    }
+    
+    return {
+      ...token,
+      ...transformed
+    }
+  }
+  
+  stringify(token, options = {}) {
+    return this.options.schema.validate(token, {strict: true}).then(validatedToken => {
+      const { expires, expiresIn } = this.calculateExpiration(options.expiresIn)
+      
+      validatedToken = {
+        exp: expires,
+        ...this.encryptOrDecryptProps(validatedToken, 'encrypt')
+      }
+
+      return {
+        tokenStr: jwt.sign(validatedToken, this.secret),
+        expiresIn
+      }
+    })
+  }
+  
+  parse(tokenStr) {
+    let verifiedToken
+    try {
+      verifiedToken = jwt.verify(tokenStr, this.secret)
+    } catch(err) {
+      return Promise.reject(err)
+    }
+    
+    verifiedToken = this.encryptOrDecryptProps(verifiedToken, 'decrypt')
+    
+    return this.options.schema.validate(verifiedToken)
+  }
+}
+
+class AccessToken extends Token {
+  constructor(secret, options = {}) {
+    const schema = yup.object().noUnknown().shape({
+      username: yup.string().required(),
+      scope: yupx.scope().required(),
+      client: yup.string().required(),
+      exp: yup.number(),
+      iat: yup.number(),
+      session: yup.object().required()
+    })
+
+    super(secret, {
+      schema, 
+      encryptProps: 'session', 
+      expiresIn: Number(env.TOKEN_EXPIRATION),
+      ...options
+    })
+  }
+}
+
+class RefreshToken extends Token { 
+  constructor(secret, options = {}) {
+    const schema = yup.object().noUnknown().shape({
+      credentials: yup.object().required(),
+      scope: yupx.scope().required()
+    })
+    
+    super(secret, {schema, encryptProps: 'credentials', ...options})
+  }
+}
+
+class AuthorizationCode extends Token {
+  constructor(secret, options = {}) {
+    const schema = yup.object().noUnknown().shape({
+      credentials: yup.object().required(),
+      scope: yupx.scope().required(),
+      uri: yup.string().required()
+    })
+    
+    super(secret, {schema, encryptProps: 'credentials', ...options})
+  }
+}
+
+AccessToken.default = new AccessToken()
+RefreshToken.default = new RefreshToken()
+AuthorizationCode.default = new AuthorizationCode()
+
+for(let clazz of [AccessToken, RefreshToken, AuthorizationCode]) {
+  const def = new clazz()
+  clazz.stringify = def.stringify
+  clazz.parse = def.parse
+}
+
+export { Token, AccessToken, RefreshToken, AuthorizationCode }
---
+++ test/tokens.test.js
@@ -0,0 +1,172 @@
+import jwt from 'jsonwebtoken'
+import createEncrypter from 'object-encrypter'
+import yup from 'yup'
+
+import { expect } from 'test-setup'
+
+import { Token } from 'tokens'
+
+describe('Token', () => {
+  const tokener = new Token('some_secret', { encryptProps: 'secret' })
+  const encrypter = createEncrypter('some_secret')
+  const sampleToken = {
+    username: 'user',
+    scope: ['scope'],
+    client: 'client',
+    secret: { key: 'value' }
+  }
+  
+  describe('stringify()', () => {
+    it('should resolve when called with a valid token', () => {
+      const result = tokener.stringify(sampleToken)
+      
+      return expect(result).to.eventually.be.resolved
+    })
+    
+    it('should resolve with a valid JWT', () => {
+      const result = tokener.stringify(sampleToken)
+        .then(r => r.tokenStr)
+        .then(jwt.decode)
+        
+      return expect(result.log()).to.eventually.be.not.null
+    })
+    
+    it('should include the expiration date if one was provided', () => {
+      const result = tokener.stringify({...sampleToken, exp: 3600})
+        .then(r => r.tokenStr)
+        .then(jwt.decode)
+        
+      expect(result).to.eventually.have.property('exp', 3600)
+    })
+    
+    it('should include the default expiration date if there is one', () => {
+      const result = new Token('some_secret', {expiresIn: 3600})
+        .stringify(sampleToken)
+        .then(r => r.tokenStr)
+        .then(jwt.decode)
+
+      return expect(result).to.eventually.have.property('exp').and.is.a('number')
+    })
+    
+    it('should override the default expiration with the provided one', () => {
+      const result = new Token('some_secret', {expiresIn: 3600})
+        .stringify({...sampleToken, exp: 999999})
+        .then(r => r.tokenStr)
+        .then(jwt.decode)
+
+      expect(result).to.eventually.have.property('exp', 999999)
+    })
+    
+    it('should return the expiration when there is one', () => {
+      const now = Math.floor(Date.now() / 1000)
+      const result = new Token('some_secret')
+        .stringify({...sampleToken, exp: now + 1000})
+        .then(r => r.expiresIn)
+        
+      expect(result).to.eventually.equal(1000)
+    })
+    
+    it('should include the provided properties in the JWT payload', () => {
+      const result = tokener.stringify(sampleToken)
+        .then(r => r.tokenStr)
+        .then(jwt.decode)
+        
+      return Promise.all([
+        expect(result).to.eventually.have.property('client', 'client'),
+        expect(result).to.eventually.have.property('username', 'user')
+      ])
+    })
+    
+    it('should encrypt any properties that are specified', () => {
+      const result = tokener.stringify(sampleToken)
+        .then(r => r.tokenStr)
+        .then(jwt.decode)
+        .then(decodedToken => encrypter.decrypt(decodedToken.secret))
+        
+      return expect(result).to.eventually.deep.equal(sampleToken.secret)
+    })
+    
+    it('should have signed the JWT with the provided secret', () => {
+      const result = tokener.stringify(sampleToken)
+        .then(r => r.tokenStr)
+        .then(tokenStr => jwt.verify(tokenStr, 'some_secret'))
+        
+      return expect(result).to.eventually.be.not.null
+    })
+    
+    it('should reject if the provided token does not match the schema', () => {
+      const tokener = new Token('some_secret', { 
+        schema: yup.object().shape({required_property: yup.mixed().required()})
+      })
+      const result = tokener.stringify({ username: 'user', client: 'client' })
+      
+      return expect(result.log()).to.eventually.be.rejected
+    })
+  })
+  
+  describe('parse()', () => {
+    it('should resolve with a valid token object when given a valid token string', () => {
+      const result = tokener.stringify(sampleToken)
+        .then(r => r.tokenStr)
+        .then(tokener.parse)
+        
+      return expect(result).to.eventually.be.not.null
+    })
+    
+    it('should return the properties in the token', () => {
+      const result = tokener.stringify(sampleToken)
+        .then(r => r.tokenStr)
+        .then(tokener.parse)
+        
+      return Promise.all([
+        expect(result).to.eventually.have.property('username', 'user'),
+        expect(result).to.eventually.have.property('client', 'client')
+      ])
+    })
+    
+    it('should decrypt any properties that were encrypted', () => {
+      const result = tokener.stringify(sampleToken)
+        .then(r => r.tokenStr)
+        .then(tokener.parse)
+        .then(t => t.session)
+      
+      return expect(result).to.eventually.deep.equal(sampleToken.session)
+    })
+    
+    it('should reject if the token doesn\'t match the schema', () => {
+      const tokener = new Token('some_secret', {
+        schema: yup.object().shape({ requiredProperty: yup.mixed().required() })
+      })
+      const badToken = jwt.sign({isValidToken: false}, 'some_secret')
+      const result = tokener.parse(badToken)
+      
+      return expect(result.log()).to.eventually.be.rejected
+    })
+    
+    it('should reject if the token wasn\'t signed with the correct secret', () => {
+      const badToken = jwt.sign(sampleToken, 'wrong_secret')
+      const result = tokener.parse(badToken)
+      
+      return expect(result.log()).to.eventually.be.rejected      
+    })
+    
+    it('should reject if the token is garbage', () => {
+      const badToken = 'NOT_A_VALID_TOKEN!'
+      const result = tokener.parse(badToken)
+      
+      return expect(result.log()).to.eventually.be.rejected
+    })
+    
+    it('should reject if the token is expired', () => {
+      const badToken = {...sampleToken,
+        iat: Date.now() - 100,
+        exp: 10
+      }
+      
+      const result = tokener.stringify(badToken)
+        .then(tokener.parse)
+        
+      expect(result.log()).to.eventually.be.rejected
+    })
+  })
+})
