58c43e88c8bd6f74d3d9ad949cb4cff05588026d
Jon Scheiding <jonscheiding@gmail.com>
2016-09-27 23:31:12 -0400

    Major overkill introducing webpack to reduce garbage in login template

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import { logger } from 'logging'
 import { express, passport } from 'config'
-
 import oauth2 from 'oauth2'
 import api from 'api'
+import webpack from 'style/webpack.config'
 
 const app = express()
 app.use(passport.initialize())
+app.use(webpack())
+
 app.use('/oauth2', oauth2)
 app.use('/api', api)
 
