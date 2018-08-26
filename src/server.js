33aa66823a13babbe569cbe6309e8e8808321cfb
Jon Scheiding <jscheiding@certicasolutions.com>
2018-08-26 11:07:46 -0400

    Added option to require SSL

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import enforce from 'express-sslify'
+
 import { logger } from 'logging'
 import { express, passport } from 'config'
 import oauth2 from 'oauth2'
@@ -5,6 +7,11 @@ import api from 'api'
 import webpack from 'style/webpack.config'
 
 const app = express()
+
+if(process.env.REQUIRE_SSL) {
+  app.use(enforce.HTTPS())
+}
+
 app.use(passport.initialize())
 app.use(webpack())
 
